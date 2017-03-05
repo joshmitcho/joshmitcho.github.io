b8e4d766eb968f11c2c65c51ca04bdc996acf6e8
josh <josh@joshpmitchell.ca>
2017-03-05 01:09:44 -0500

    Add mailto: Button

--- js/contact.js
+++ js/contact.js
@@ -1,12 +1,12 @@
 document.write('\
 	<section id="contact">\
-		<div class="container">\
+		<div class="container text-center">\
 			<div class="row">\
-				<div class="col-lg-12 text-center">\
+				<div class="col-lg-12">\
                     <h2>Contact</h2> <br>\
                 </div>\
 			</div>\
-			<form id="contactform" method="POST" class="text-center">\
+			<form id="contactform" method="POST">\
 				<input type="text" name="name" placeholder="Your name"> <br>\
 				<input type="email" name="_replyto" placeholder="Your email"> <br>\
 				<input type="hidden" name="_subject" value="Website contact" />\
@@ -15,6 +15,8 @@ document.write('\
 				<input type="hidden" name="_next" value="/thanks.html" /> <br>\
 				<input type="submit" value="Send">\
 			</form>\
+			or<br>\
+			<a href="mailto:josh@joshpmitchell.ca" target="_blank" class="btn btn-lg btn-success">Email Me Directly &nbsp<i class="fa fa-envelope" aria-hidden="true"></i></a>\
         </div>\
     </section>\
 ');
